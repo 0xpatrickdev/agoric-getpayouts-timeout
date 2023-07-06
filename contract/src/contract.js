60a2262bfd6feb6ed6c86c5c03e6bc52ef370ce1
Patrick Cooney <patrick@lari.finance>
2023-07-06 15:39:07 -0400

    feaet: simple escrow contract example to demonstrate potential getPayouts bug

--- contract/src/contract.js
+++ contract/src/contract.js
@@ -1,58 +1,67 @@
 // @ts-check
 /* global harden */
 import '@agoric/zoe/exported.js';
-import { AmountMath } from '@agoric/ertp';
+import { M } from '@agoric/store';
+import { atomicTransfer } from '@agoric/zoe/src/contractSupport/index.js';
+import { E } from '@endo/eventual-send';
 import { Far } from '@endo/marshal';
 
+const makeWaker = (name, func) => {
+  return Far(name, {
+    wake: (timestamp) => func(timestamp),
+  });
+};
+
 /**
- * This is a very simple contract that creates a new issuer and mints payments
- * from it, in order to give an example of how that can be done.  This contract
- * sends new tokens to anyone who has an invitation.
- *
- * The expectation is that most contracts that want to do something similar
- * would use the ability to mint new payments internally rather than sharing
- * that ability widely as this one does.
- *
- * To pay others in tokens, the creator of the instance can make
- * invitations for them, which when used to make an offer, will payout
- * the specified amount of tokens.
+ * This is a simple contract that takes a collateralAmount, and locks it in
+ * in the contract until the expirationTime.
  *
  * @type {ContractStartFn}
  */
 const start = async (zcf) => {
-  // Create the internal token mint for a fungible digital asset. Note
-  // that 'Tokens' is both the keyword and the allegedName.
-  const zcfMint = await zcf.makeZCFMint('Tokens');
-  // AWAIT
+  const { zcfSeat: collateralSeat } = zcf.makeEmptySeatKit();
+  const { expirationTime, timerService, collateralAmount } = zcf.getTerms();
 
-  // Now that ZCF has saved the issuer, brand, and local amountMath, they
-  // can be accessed synchronously.
-  const { issuer, brand } = zcfMint.getIssuerRecord();
+  const expirationWaker = (depositorSeat) =>
+    makeWaker('expirationWaker', (_timestamp) => {
+      atomicTransfer(zcf, collateralSeat, depositorSeat, {
+        Collateral: collateralSeat.getAmountAllocated('Collateral'),
+      });
+      depositorSeat.exit();
+      zcf.shutdown(`Escrowed Collateral is now claimable.`);
+    });
 
   /** @type {OfferHandler} */
-  const mintPayment = (seat) => {
-    const amount = AmountMath.make(brand, 1000n);
-    // Synchronously mint and allocate amount to seat.
-    zcfMint.mintGains(harden({ Token: amount }), seat);
-    // Exit the seat so that the user gets a payout.
-    seat.exit();
+  const lockCollateral = (depositorSeat) => {
+    atomicTransfer(zcf, depositorSeat, collateralSeat, {
+      Collateral: depositorSeat.getAmountAllocated('Collateral'),
+    });
+
+    E(timerService).setWakeup(expirationTime, expirationWaker(depositorSeat));
     // Since the user is getting the payout through Zoe, we can
     // return anything here. Let's return some helpful instructions.
-    return 'Offer completed. You should receive a payment from Zoe';
+
+    return Far('depositorFacet', {
+      getCollateralAmount: () =>
+        collateralSeat.getAmountAllocated('Collateral'),
+    });
   };
 
   const creatorFacet = Far('creatorFacet', {
-    // The creator of the instance can send invitations to anyone
-    // they wish to.
-    makeInvitation: () => zcf.makeInvitation(mintPayment, 'mint a payment'),
-    getTokenIssuer: () => issuer,
+    makeLockCollateralInvitation: () =>
+      zcf.makeInvitation(
+        lockCollateral,
+        'lockCollateral',
+        { expirationTime },
+        harden({
+          give: { Collateral: M.gte(collateralAmount) },
+          want: {},
+          exit: M.any(),
+        }),
+      ),
   });
 
-  const publicFacet = Far('publicFacet', {
-    // Make the token issuer public. Note that only the mint can
-    // make new digital assets. The issuer is ok to make public.
-    getTokenIssuer: () => issuer,
-  });
+  const publicFacet = Far('publicFacet', {});
 
   // Return the creatorFacet to the creator, so they can make
   // invitations for others to get payments of tokens. Publish the
--- contract/test/test-contract.js
+++ contract/test/test-contract.js
@@ -8,44 +8,180 @@ import bundleSource from '@endo/bundle-source';
 
 import { E } from '@endo/eventual-send';
 import { makeFakeVatAdmin } from '@agoric/zoe/tools/fakeVatAdmin.js';
-import { makeZoeKit } from '@agoric/zoe';
-import { AmountMath } from '@agoric/ertp';
+import { makeZoeKitForTest } from '@agoric/zoe/tools/setup-zoe.js';
+import { makeIssuerKit, AmountMath } from '@agoric/ertp';
+import { buildManualTimer } from '@agoric/swingset-vat/tools/manual-timer.js';
 
 const filename = new URL(import.meta.url).pathname;
 const dirname = path.dirname(filename);
 
 const contractPath = `${dirname}/../src/contract.js`;
+const bundleId = 'getPayoutsTest';
 
-test('zoe - mint payments', async (t) => {
-  const { zoeService } = makeZoeKit(makeFakeVatAdmin().admin);
-  const feePurse = E(zoeService).makeFeePurse();
-  const zoe = E(zoeService).bindDefaultFeePurse(feePurse);
+const setupTest = async (t) => {
+  const { admin: fakeVatAdmin, vatAdminState } = makeFakeVatAdmin();
+  const { zoeService: zoe } = makeZoeKitForTest(fakeVatAdmin);
+  const mockChainTimerService = buildManualTimer(console.log);
 
-  // pack the contract
-  const bundle = await bundleSource(contractPath);
+  // make test currency for alice
+  const moolahKit = makeIssuerKit('Moolah');
+  const oneThousandMoolah = AmountMath.make(moolahKit.brand, 1_000n);
+  const zeroMoolah = AmountMath.make(moolahKit.brand, 0n);
+  const moolahPayment = moolahKit.mint.mintPayment(oneThousandMoolah);
 
-  // install the contract
-  const installation = E(zoe).install(bundle);
+  const contractBundle = await bundleSource(contractPath);
+  vatAdminState.installBundle(bundleId, contractBundle);
+  const installation = await E(zoe).install(contractBundle);
+  return {
+    moolahKit,
+    oneThousandMoolah,
+    zeroMoolah,
+    moolahPayment,
+    mockChainTimerService,
+    installation,
+    zoe,
+  };
+};
 
-  const { creatorFacet, instance } = await E(zoe).startInstance(installation);
+test('lock and claim collateral - happy path ', async (t) => {
+  const {
+    moolahKit,
+    oneThousandMoolah,
+    moolahPayment,
+    mockChainTimerService,
+    installation,
+    zoe,
+  } = await setupTest(t);
 
-  // Alice makes an invitation for Bob that will give him 1000 tokens
-  const invitation = E(creatorFacet).makeInvitation();
+  const issuerKeywordRecord = harden({ Collateral: moolahKit.issuer });
+  const terms = {
+    collateralAmount: oneThousandMoolah,
+    expirationTime: 5n,
+    timerService: mockChainTimerService,
+  };
 
-  // Bob makes an offer using the invitation
-  const seat = E(zoe).offer(invitation);
+  const { creatorFacet } = await E(zoe).startInstance(
+    installation,
+    issuerKeywordRecord,
+    terms,
+  );
 
-  const paymentP = E(seat).getPayout('Token');
+  // Alice makes an invitation to escrow collateral
+  const invitation = E(creatorFacet).makeLockCollateralInvitation();
 
-  // Let's get the tokenIssuer from the contract so we can evaluate
-  // what we get as our payout
-  const publicFacet = E(zoe).getPublicFacet(instance);
-  const tokenIssuer = E(publicFacet).getTokenIssuer();
-  const tokenBrand = await E(tokenIssuer).getBrand();
+  // Alice makes an offer using the invitation
+  const proposal = harden({ give: { Collateral: oneThousandMoolah } });
+  const payment = harden({ Collateral: moolahPayment });
+  const aliceSeat = E(zoe).offer(invitation, proposal, payment);
 
-  const tokens1000 = AmountMath.make(tokenBrand, 1000n);
-  const tokenPayoutAmount = await E(tokenIssuer).getAmountOf(paymentP);
+  const depositorFacet = await E(aliceSeat).getOfferResult();
+  const amt = await E(depositorFacet).getCollateralAmount();
+  t.deepEqual(
+    amt,
+    oneThousandMoolah,
+    "Collateral seat should contain Alice's Moolah",
+  );
 
-  // Bob got 1000 tokens
-  t.deepEqual(tokenPayoutAmount, tokens1000);
+  // fast-forward to the expiration time
+  mockChainTimerService.advanceTo(terms.expirationTime);
+  const { Collateral: alicesClaimedCollateral } = await E(
+    aliceSeat,
+  ).getPayouts();
+  t.deepEqual(
+    await moolahKit.issuer.getAmountOf(alicesClaimedCollateral),
+    oneThousandMoolah,
+    'Alice should have her Moolah back.',
+  );
+});
+
+test('lock and claim collateral too early - promise timeout ', async (t) => {
+  const {
+    moolahKit,
+    oneThousandMoolah,
+    zeroMoolah,
+    moolahPayment,
+    mockChainTimerService,
+    installation,
+    zoe,
+  } = await setupTest(t);
+
+  const issuerKeywordRecord = harden({ Collateral: moolahKit.issuer });
+  const terms = {
+    collateralAmount: oneThousandMoolah,
+    expirationTime: 5n,
+    timerService: mockChainTimerService,
+  };
+
+  const { creatorFacet } = await E(zoe).startInstance(
+    installation,
+    issuerKeywordRecord,
+    terms,
+  );
+
+  // Alice makes an invitation to escrow collateral
+  const invitation = E(creatorFacet).makeLockCollateralInvitation();
+
+  // Alice makes an offer using the invitation
+  const proposal = harden({ give: { Collateral: oneThousandMoolah } });
+  const payment = harden({ Collateral: moolahPayment });
+  const aliceSeat = E(zoe).offer(invitation, proposal, payment);
+
+  // fast-forward to 1s before expiration time
+  mockChainTimerService.advanceTo(terms.expirationTime - 1n);
+  // .getPayouts() results in  Error: Promise returned by test never resolved
+  // at process.emit (node:events:513:28)
+  const { Collateral: alicesClaimedCollateral } = await E(
+    aliceSeat,
+  ).getPayouts();
+  t.deepEqual(
+    await moolahKit.issuer.getAmountOf(alicesClaimedCollateral),
+    zeroMoolah,
+    'Alice should not have her Moolah back yet.',
+  );
+});
+
+test('lock and claim collateral too early - no promise timeout when tryExit is called first ', async (t) => {
+  const {
+    moolahKit,
+    oneThousandMoolah,
+    zeroMoolah,
+    moolahPayment,
+    mockChainTimerService,
+    installation,
+    zoe,
+  } = await setupTest(t);
+
+  const issuerKeywordRecord = harden({ Collateral: moolahKit.issuer });
+  const terms = {
+    collateralAmount: oneThousandMoolah,
+    expirationTime: 5n,
+    timerService: mockChainTimerService,
+  };
+
+  const { creatorFacet } = await E(zoe).startInstance(
+    installation,
+    issuerKeywordRecord,
+    terms,
+  );
+
+  // Alice makes an invitation to escrow collateral
+  const invitation = E(creatorFacet).makeLockCollateralInvitation();
+
+  // Alice makes an offer using the invitation
+  const proposal = harden({ give: { Collateral: oneThousandMoolah } });
+  const payment = harden({ Collateral: moolahPayment });
+  const aliceSeat = E(zoe).offer(invitation, proposal, payment);
+
+  // fast-forward to 1s before expiration time
+  mockChainTimerService.advanceTo(terms.expirationTime - 1n);
+  // call tryExit first, getPayouts fires successfully
+  await E(aliceSeat).tryExit();
+  const { Collateral: alicesClaimedCollateral } = await E(
+    aliceSeat,
+  ).getPayouts();
+  t.deepEqual(
+    await moolahKit.issuer.getAmountOf(alicesClaimedCollateral),
+    zeroMoolah,
+    'Alice should not have her Moolah back yet.',
+  );
 });
